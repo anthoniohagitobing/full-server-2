b0abc24332a6f62049ded67f24bcba8d53c16c1a
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-10-23 14:58:53 +0900

    feat: add deployment for react7

--- server/server.js
+++ server/server.js
@@ -17,15 +17,17 @@ app.use(cors());
 // });
   // manual way of setting cors
   // Source: https://codedamn.com/news/backend/how-to-fix-cors-error
-app.use(express.static(path.join(__dirname + "/../client", "public")));
-
+app.use(express.static(path.join(__dirname + "/../client", "build")));
+  // this will make the server serve the static files from the react client public folder (made by build command)
+  // if you access home, you will get this. 
+  // Because of this reason, you need to set address to something other than home
 
 // IMPORTING MODEL FROM KNEX
 const model = require("./src/model");
-
+// teset
 
 // SENDING DATA
-app.get("/", async (req, res) => {
+app.get("/get", async (req, res) => {
   // console.log("test");
   const data = await model.getAllAccount();
   console.log(data);
