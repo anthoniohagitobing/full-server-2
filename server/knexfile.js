12b70599a5688e0a115804b447533ddbd968b3c3
Anthonio Hagi Tobing <133083562+anthoniohagitobing@users.noreply.github.com>
2023-10-23 12:09:51 +0900

    feat: test backend for render

--- server/knexfile.js
+++ server/knexfile.js
@@ -7,9 +7,9 @@ require("dotenv").config({
  */
 module.exports = {
   client: "postgresql",
-  connection: process.env.DB_URL || {
-    host: process.env.DB_HOST || "127.0.0.1",
-    port: process.env.DB_PORT || 5432,
+  connection: process.env.DATABASE_URL || {
+    // host: process.env.DB_HOST || "127.0.0.1",
+    // port: process.env.DB_PORT || 5432,
     database: process.env.DB_NAME,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
